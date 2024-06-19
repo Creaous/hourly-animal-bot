da7666bc8e0b4ba323e7908cd89f0bcf8ba46cec
Creaous <68230846+Creaous@users.noreply.github.com>
2024-06-20 08:27:36 +0930

    damn you

--- scheduler.py
+++ scheduler.py
@@ -22,7 +22,7 @@ def run_task(type):
     write_timing(type)
     subprocess.Popen(["python", f"{type}.py"])
 
-schedule.every().at_time("00:00").do(run_task, "unsplash")
+schedule.every().at_time(datetime.datetime.strptime("00:00", "%H:%M")).do(run_task, "unsplash")
 
 for time in ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]:
     print("Twitter task scheduled for {}".format(time))
